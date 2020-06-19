737ea85af8f4bbbc001cb2fecdea3250d4730f30
SultanFranco09 <s.syidaliev@gmail.com>
2020-06-20 03:36:35 +0600

    added yasg

--- agrarie/yasg.py
+++ agrarie/yasg.py
@@ -15,7 +15,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('documentations(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('documentations/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('redocumentations/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
