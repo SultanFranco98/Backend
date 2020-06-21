01a96f4071ddd7c344c67965bb65d4aa1df6fa60
SultanFranco09 <s.syidaliev@gmail.com>
2020-06-22 01:08:02 +0600

    create urls,serializers and connect application

--- agrarie/urls.py
+++ agrarie/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
                   path('jet/', include('jet.urls', 'jet')),
                   path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
                   path('admin/', admin.site.urls),
-                  path('api-auth/', include('rest_framework.urls')),
+                  # path('api-auth/', include('rest_framework.urls')),
                   path('api/', include('users.urls')),
                   path('api/', include('forums.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
