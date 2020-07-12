5bac84605978e91919a1d3116c2655e9a1f14d4e
SultanFranco98 <s.syidaliev@gmail.com>
2020-07-13 02:51:51 +0600

    remove chat applications

--- agrarie/routing.py
+++ agrarie/routing.py
@@ -1,17 +1,17 @@
-from django.conf.urls import url
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
-from chat.consumer import ChatConsumer
-
-application = ProtocolTypeRouter({
-    'websocket': AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                [
-                    url(r"^(?P<email>[\w.@+-]+)", ChatConsumer),
-                ]
-            )
-        )
-    )
-})
+# from django.conf.urls import url
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+# from chat.consumer import ChatConsumer
+#
+# application = ProtocolTypeRouter({
+#     'websocket': AllowedHostsOriginValidator(
+#         AuthMiddlewareStack(
+#             URLRouter(
+#                 [
+#                     url(r"^(?P<email>[\w.@+-]+)", ChatConsumer),
+#                 ]
+#             )
+#         )
+#     )
+# })
