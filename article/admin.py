5fc695f2f031e27f238ad6d8b39d895deaf21e61
OLOLO6 <desm606@gmail.com>
2020-07-22 14:08:49 +0600

    added addition content to article

--- article/admin.py
+++ article/admin.py
@@ -6,13 +6,19 @@ admin.site.site_title = "Агро Консультирование"
 admin.site.index_title = "Агро Консультирование"
 
 
+class AdditionInline(admin.TabularInline):
+    model = ArticleAddition
+    fields = ('subtitle', 'subtext')
+
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'status', 'category', 'user', 'votes']
     list_filter = ['user__email', 'user__first_name', 'user__last_name', 'status', 'category', 'subcategory', 'types',
                    'subtypes', 'votes']
     search_fields = ['user__email', 'user__first_name', 'user__last_name']
-    readonly_fields = ['pub_date']
+    readonly_fields = ['pub_date', 'votes']
     ordering = ['-pub_date']
+    inlines = (AdditionInline,)
 
 
 class VoteAdmin(admin.ModelAdmin):
--- article/serializers.py
+++ article/serializers.py
@@ -13,23 +13,39 @@ class VoteSerializer(serializers.ModelSerializer):
         read_only_fields = ['user', 'pub_date']
 
 
+class ArticleAdditionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleAddition
+        fields = ['id', 'article', 'subtitle', 'subtext']
+        read_only_fields = ['article']
+
+
 class ArticleListSerializer(serializers.ModelSerializer):
+    additions = ArticleAdditionSerializer(read_only=True, many=True)
     votes = serializers.IntegerField(source='total_votes', read_only=True)
     user = UsersListSerializer(read_only=True, many=False)
 
     class Meta:
         model = Article
-        fields = ['id', 'user', 'category', 'subcategory', 'types', 'subtypes', 'title', 'text', 'pub_date', 'status',
+        fields = ['id', 'user', 'category', 'subcategory', 'types', 'subtypes', 'title', 'text', 'additions', 'pub_date', 'status',
                   'votes']
-        read_only_fields = ['user', 'pub_date', 'votes', 'status']
+        read_only_fields = ['user', 'pub_date', 'votes', 'status', 'additions']
 
 
 class ArticleDetailSerializer(serializers.ModelSerializer):
+    additions = ArticleAdditionSerializer(many=True)
     votes = serializers.IntegerField(source='total_votes', read_only=True)
     user = UsersListSerializer(read_only=True, many=False)
 
     class Meta:
         model = Article
-        fields = ['id', 'user', 'category', 'subcategory', 'types', 'subtypes', 'title', 'text', 'pub_date', 'status',
-                  'votes']
+        fields = ['id', 'user', 'category', 'subcategory', 'types', 'subtypes', 'title', 'text', 'additions',
+                  'pub_date', 'status', 'votes']
         read_only_fields = ['user', 'pub_date', 'votes', 'status']
+
+    def create(self, validated_data):
+        additions_data = validated_data.pop('additions')
+        article = Article.objects.create(**validated_data)
+        for addition_data in additions_data:
+            ArticleAddition.objects.create(article=article, **addition_data)
+        return article
