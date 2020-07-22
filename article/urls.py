8fb7dd3ffcf92e8bec713b0f1d3abba7166e1c90
OLOLO6 <desm606@gmail.com>
2020-07-22 15:13:24 +0600

    added new and populare article

--- article/urls.py
+++ article/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('articles/delete/<int:pk>', ArticleViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'})),
     path('articles/create', ArticleViewSet.as_view({'post': 'create'})),
 
+    path('popular-articles/', PopularArticleViewSet.as_view({'get': 'list'})),
+    path('new-articles/', NewArticleViewSet.as_view({'get': 'list'})),
+
     path('votes/create/<int:pk>', VoteViewSet.as_view({'post': 'create'})),
     path('votes/edit/<int:pk>', VoteViewSet.as_view({'get': 'retrieve', 'put': 'update'})),
     path('votes/delete/<int:pk>', VoteViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'})),
