5d8b08801f7659365d926c43d540f15ce44ea44b
OLOLO6 <desm606@gmail.com>
2020-07-23 02:02:56 +0600

    added translation for info app

--- informations/apps.py
+++ informations/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class InformationsConfig(AppConfig):
     name = 'informations'
-    verbose_name = 'Информация на сайте'
+    verbose_name = _('Информация на сайте')
