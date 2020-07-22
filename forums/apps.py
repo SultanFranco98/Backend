4ea8a225843fdae3e203f11bdc2b0652b61e6a2b
OLOLO6 <desm606@gmail.com>
2020-07-23 02:01:30 +0600

    added translation for forum

--- forums/apps.py
+++ forums/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ForumsConfig(AppConfig):
     name = 'forums'
-    verbose_name = 'Форум'
+    verbose_name = _('Форум')
---
+++ forums/translation.py
@@ -0,0 +1,38 @@
+from modeltranslation.translator import register, TranslationOptions
+from .models import *
+
+
+@register(Category)
+class CategoryTranslationOptions(TranslationOptions):
+    fields = ('title', 'description')
+    required_languages = ('ru', 'kg')
+
+
+@register(SubCategory)
+class SubCategoryTranslationOptions(TranslationOptions):
+    fields = ('title',)
+    required_languages = ('ru', 'kg')
+
+
+@register(Types)
+class TypesTranslationOptions(TranslationOptions):
+    fields = ('title',)
+    required_languages = ('ru', 'kg')
+
+
+@register(SubTypes)
+class SubTypesTranslationOptions(TranslationOptions):
+    fields = ('title',)
+    required_languages = ('ru', 'kg')
+
+
+@register(Forum)
+class ForumTranslationOptions(TranslationOptions):
+    fields = ('title',)
+    required_languages = ('ru', 'kg')
+
+
+@register(Comment)
+class ForumTranslationOptions(TranslationOptions):
+    fields = ('description',)
+    required_languages = ('ru', 'kg')
