5bbf4790da9a00d4fa8b843d23b862c1180433f7
SultanFranco98 <s.syidaliev@gmail.com>
2020-07-25 00:51:31 +0600

    added performe_create

--- users/views.py
+++ users/views.py
@@ -54,6 +54,7 @@ class CertificateViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data, many=True)
         serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
