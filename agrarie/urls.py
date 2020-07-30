263656f7a913b8e6a00a3255c5fdeff7ab6ffa79
OLOLO6 <desm606@gmail.com>
2020-07-30 21:42:29 +0600

    changed translation urls

--- agrarie/urls.py
+++ agrarie/urls.py
@@ -3,34 +3,23 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from .yasg import urlpatterns as doc_urls
-<<<<<<< HEAD
+from django.conf.urls.i18n import i18n_patterns
+
 urlpatterns = [
                   path('jet/', include('jet.urls', 'jet')),
                   path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
-                  path('admin/', admin.site.urls),
-                  # path('api-auth/', include('rest_framework.urls')),
-                  path('api/', include('users.urls')),
-                  path('api/', include('forums.urls')),
-                  path('api/', include('article.urls')),
                   path('api/auth/', include('djoser.urls')),
                   path('api/', include('informations.urls')),
                   path('auth/', include('rest_framework_social_oauth2.urls')),
+                  # path('api-auth/', include('rest_framework.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = i18n_patterns(
-    path('jet/', include('jet.urls', 'jet')),
-    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
+urlpatterns += doc_urls
+
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
-    # path('api-auth/', include('rest_framework.urls')),
     path('api/', include('users.urls')),
     path('api/', include('forums.urls')),
     path('api/', include('article.urls')),
-    path('api/auth/', include('djoser.urls')),
-    path('api/', include('informations.urls')),
-    path('auth/', include('rest_framework_social_oauth2.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> origin/second
-
-urlpatterns += doc_urls
+    path('i18n/', include('django.conf.urls.i18n')),
+)
