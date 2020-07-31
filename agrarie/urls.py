cc9e07319d218d29d747c15f5f660f7ab4a79171
SultanFranco98 <s.syidaliev@gmail.com>
2020-07-31 14:10:56 +0600

    change agrarie app urls

--- agrarie/urls.py
+++ agrarie/urls.py
@@ -6,23 +6,21 @@ from .yasg import urlpatterns as doc_urls
 from django.conf.urls.i18n import i18n_patterns
 from agrarie.search import GlobalSearchAPIView
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
+                  path('admin/', admin.site.urls),
+                  path('api/', include('users.urls')),
+                  path('api/text/', GlobalSearchAPIView.as_view()),
+                  path('api/', include('forums.urls')),
+                  path('api/', include('article.urls')),
+                  path('api/', include('categories.urls')),
+                  path('i18n/', include('django.conf.urls.i18n')),
                   path('jet/', include('jet.urls', 'jet')),
                   path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
                   path('api/auth/', include('djoser.urls')),
                   path('api/', include('informations.urls')),
                   path('auth/', include('rest_framework_social_oauth2.urls')),
                   # path('api-auth/', include('rest_framework.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += doc_urls
 
-urlpatterns += i18n_patterns(
-    path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
-    path('api/text/', GlobalSearchAPIView.as_view()),
-    path('api/', include('forums.urls')),
-    path('api/', include('article.urls')),
-    path('api/', include('categories.urls')),
-    path('i18n/', include('django.conf.urls.i18n')),
-)
