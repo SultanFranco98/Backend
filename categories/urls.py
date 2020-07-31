db8357a6b6355d71a7e223dec7f7345a288fd9dc
SultanFranco98 <s.syidaliev@gmail.com>
2020-07-31 19:28:06 +0600

    remove url specialty create

--- categories/urls.py
+++ categories/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('subtypes/<int:pk>/', SubTypesViewSet.as_view({'get': 'retrieve'})),
     path('specialty/', SpecialtyViewSet.as_view({'get': 'list'})),
     path('specialty/<int:pk>/', SpecialtyViewSet.as_view({'get': 'retrieve'})),
-    path('specialty/create', SpecialtyViewSet.as_view({'post': 'create'})),
 ]
